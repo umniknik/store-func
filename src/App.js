154aefe7a3ca9655919d007d1a2f714b505c7629
umniknik <kaaaaaljan@mail.ru>
2024-08-02 04:30:06 +0300

    ДЗ к занятию "Компоненты - Сделана первая часть (функциональный компонент).

---
+++ src/App.js
@@ -0,0 +1,48 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  )
+}
+
+function ShopItemFunc({ item }) {
+  return (
+    <div className='main-content'>
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className='description'>{item.descriptionFull}</div>
+      <div className='highlight-window mobile'><div className='highlight-overlay'></div></div>
+      <div className='divider'></div>
+      <div className='purchase-info'>
+        <div className='price'>{item.currency + item.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // ...
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+                emitFile: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+    // ...
+  }
+  
